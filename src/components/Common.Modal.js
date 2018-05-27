5aece3036ab6bd734f20e47291cc35b5d241afcd
Pierre Reimertz <pierre.reimertz@gmail.com>
2018-05-28 00:50:41 +0200

    fixes ie11 bug max-width bug with the modal

--- src/components/Common.Modal.js
+++ src/components/Common.Modal.js
@@ -6,8 +6,7 @@ import 'rodal/lib/rodal.css'
 const customStyles = {
   backgroundColor: '#2878E0',
   color: 'white',
-  width: 'auto',
-  maxWidth: '600px',
+  width: '600px',
   padding: '2rem 1rem 0 3rem',
   borderRadius: '1rem',
 }
