65fb53833e34ae6ddaa69e64dd7f08e4449eeeb8
rawknuckles <raunaq@planetscale.com>
2019-06-18 10:50:42 +0530

    Add gtm (#228)
    * Add gtm
    
    * use the actual gtm package

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,5 +80,20 @@ module.exports = {
     },
     'gatsby-plugin-netlify-cms',
     'gatsby-plugin-netlify',
+    {
+      resolve: `gatsby-plugin-google-tagmanager`,
+      options: {
+        id: 'GTM-KL98Z8C',
+
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+
+        // Specify optional GTM environment details.
+        gtmAuth: 'YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_AUTH_STRING',
+        gtmPreview: 'YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_PREVIEW_NAME',
+        dataLayerName: 'YOUR_DATA_LAYER_NAME',
+      },
+    },
   ],
 }
