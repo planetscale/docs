f32abbe2a1c99503d2e245d3b4162e7e198fb611
rawknuckles <raunaq@planetscale.com>
2019-06-20 16:10:42 +0530

    Remove unused variables in gtm config (#230)

--- gatsby-config.js
+++ gatsby-config.js
@@ -87,12 +87,7 @@ module.exports = {
 
         // Include GTM in development.
         // Defaults to false meaning GTM will only be loaded in production.
-        includeInDevelopment: false,
-
-        // Specify optional GTM environment details.
-        gtmAuth: 'YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_AUTH_STRING',
-        gtmPreview: 'YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_PREVIEW_NAME',
-        dataLayerName: 'YOUR_DATA_LAYER_NAME',
+        includeInDevelopment: true,
       },
     },
   ],
