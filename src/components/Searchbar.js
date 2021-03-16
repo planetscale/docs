f6202cb547b26364e29d7efcd202b887e99b190c
rawknuckles <mail@raunaqgupta.com>
2021-03-16 14:11:11 -0700

    update searchbar and suggestion colors (#640)

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -5,21 +5,22 @@ const SearchContainer = styled.div``
 const SearchBoxLabel = styled.label``
 
 const SearchBox = styled.input`
-  border: 1px solid var(--border-primary);
+  border: 1px solid var(--border-action);
   font-size: 14px;
   border-radius: 6px;
   outline: 0;
   padding: 0.5em 1em;
-  background-color: #272830;
+  background-color: var(--bg-secondary);
   color: var(--text-primary);
+  box-shadow: var(--tw-shadow);
 
   &:focus {
+    background-color: var(--bg-primary);
     border-color: var(--text-blue);
   }
 
   &::placeholder {
     color: var(--text-secondary);
-    font-weight: 400;
   }
 `
 
