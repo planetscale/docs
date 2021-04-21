02d7080672b18ed0748bedaaa563901e966b6ad9
rawknuckles <mail@raunaqgupta.com>
2021-04-21 14:39:51 -0700

    Click inline code block to copy (#726)

--- src/components/MDX.InlineCodeBlock.js
+++ src/components/MDX.InlineCodeBlock.js
@@ -5,8 +5,22 @@ const InlineCodeBlockContainer = styled.code`
   background-color: var(--bg-tertiary);
   padding: 4px 8px;
   border-radius: 4px;
+  border: 1px solid var(--bg-tertiary);
+
+  &:hover {
+    cursor: pointer;
+    border: 1px solid var(--bg-blue);
+  }
 `
 
 export default function InlineCodeBlock({ children }) {
-  return <InlineCodeBlockContainer>{children}</InlineCodeBlockContainer>
+  const copyToClipboard = (e) => {
+    navigator.clipboard.writeText(children)
+  }
+
+  return (
+    <InlineCodeBlockContainer as="code" onClick={copyToClipboard}>
+      {children}
+    </InlineCodeBlockContainer>
+  )
 }
