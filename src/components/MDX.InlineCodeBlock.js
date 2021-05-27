cbc976f242ac62e43f11d09321a64f7a6cc95f51
rawknuckles <mail@raunaqgupta.com>
2021-05-27 10:23:25 -0700

    Provide visual feedback on inline code block click (#860)

--- src/components/MDX.InlineCodeBlock.js
+++ src/components/MDX.InlineCodeBlock.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Check } from '@styled-icons/remix-line'
 import { styled } from './styles/stitches.config'
 
 const InlineCodeBlockContainer = styled('code', {
@@ -15,14 +16,30 @@ const InlineCodeBlockContainer = styled('code', {
   },
 })
 
+const CopiedState = styled('code', {
+  '> svg': {
+    width: '12px',
+  },
+})
+
 export default function InlineCodeBlock({ children }) {
+  const [content, setContent] = useState(children)
+
   const copyToClipboard = (e) => {
     navigator.clipboard.writeText(children)
+    setContent(
+      <CopiedState>
+        <Check /> Copied!
+      </CopiedState>
+    )
+    setTimeout(() => {
+      setContent(children)
+    }, 1000)
   }
 
   return (
     <InlineCodeBlockContainer onClick={copyToClipboard}>
-      {children}
+      {content}
     </InlineCodeBlockContainer>
   )
 }
