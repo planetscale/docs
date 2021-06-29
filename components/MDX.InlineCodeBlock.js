e219558563dcee22a6f90ccfa51e50cda35f28d6
rawknuckles <mail@raunaqgupta.com>
2021-06-29 12:42:39 -0700

    Remove hover, click states from inline code block (#944)

--- components/MDX.InlineCodeBlock.js
+++ components/MDX.InlineCodeBlock.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react'
-import { Check } from '@styled-icons/remix-line'
+import React from 'react'
 import { styled } from '../stitches.config'
 
 const InlineCodeBlockContainer = styled('code', {
@@ -8,38 +7,8 @@ const InlineCodeBlockContainer = styled('code', {
   margin: '0 4px',
   borderRadius: '4px',
   lineHeight: '28px',
-
-  '&:hover': {
-    cursor: 'pointer',
-    color: '$bgPrimary',
-    background: '$textBlue',
-  },
-})
-
-const CopiedState = styled('code', {
-  '> svg': {
-    width: '12px',
-  },
 })
 
 export default function InlineCodeBlock({ children }) {
-  const [content, setContent] = useState(children)
-
-  const copyToClipboard = (e) => {
-    navigator.clipboard.writeText(children)
-    setContent(
-      <CopiedState>
-        <Check /> Copied!
-      </CopiedState>
-    )
-    setTimeout(() => {
-      setContent(children)
-    }, 1000)
-  }
-
-  return (
-    <InlineCodeBlockContainer onClick={copyToClipboard}>
-      {content}
-    </InlineCodeBlockContainer>
-  )
+  return <InlineCodeBlockContainer>{children}</InlineCodeBlockContainer>
 }
