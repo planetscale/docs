e1fc7f649ce5e7b56de01fdf2a94846d900a88a3
Jason Long <jason@jasonlong.me>
2021-11-15 19:30:09 -0500

    Add theme toggle button

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,42 +1,31 @@
 import React from 'react'
 import meta from '../content/docs/meta.json'
 import ActiveLink from './ActiveLink'
+import Button from './Button'
+import { useDarkMode } from 'next-dark-mode'
 
 export default function Navigation() {
-  const toc = meta
+  const { darkModeActive, switchToDarkMode, switchToLightMode } = useDarkMode()
 
-  function recursiveMap(leaves) {
-    leaves.map((leaf, index) => {
-      if (leaf === 'page') {
-        return (
-          <SidenavGroup
-            key={index}
-            categoryID={category.id}
-            category={category.name}
-            pages={category.pages}
-            clickHandler={mobileTOCState ? toggleMobileTOC : () => {}}
-          ></SidenavGroup>
-        )
-      } else {
-        recursiveMap(leaf)
-      }
-    })
+  const toggleMode = () => {
+    console.log(darkModeActive)
+    if (darkModeActive) switchToLightMode()
+    else switchToDarkMode()
   }
+
+  const toc = meta
 
   function SidenavGroup({ categoryID, category, pages, clickHandler }) {
     return (
-      <div className="mb-3">
-        <strong className="block mb-1">{category}</strong>
+      <div className='mb-3'>
+        <strong className='block mb-1'>{category}</strong>
         {pages.map((page, index) => {
           const href = `/${categoryID}/${page['route']}`
 
           return (
-            <span key={index} className="block -ml-1.5">
-              <ActiveLink
-                href={href}
-                activeClassName="bg-secondary !text-primary font-medium"
-              >
-                <a className="px-1.5 rounded-sm block leading-loose py-0.5 text-secondary hover:text-primary hover:bg-secondary">
+            <span key={index} className='block -ml-1.5'>
+              <ActiveLink href={href} activeClassName='bg-secondary !text-primary font-medium'>
+                <a className='px-1.5 rounded-sm block leading-loose py-0.5 text-secondary hover:text-primary hover:bg-secondary'>
                   {page['display']}
                 </a>
               </ActiveLink>
@@ -59,6 +48,10 @@ export default function Navigation() {
           ></SidenavGroup>
         )
       })}
+      <Button size='small' variant='secondary' className='w-full' onClick={toggleMode}>
+        {darkModeActive && <>Switch to light mode</>}
+        {!darkModeActive && <>Switch to dark mode</>}
+      </Button>
     </>
   )
 }
