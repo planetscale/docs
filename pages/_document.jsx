04676a2b9c3d3358972d9ec710551ca10777444b
Jason Long <jason@jasonlong.me>
2022-02-10 10:21:12 -0500

    Update favicon colors, add favicon.ico

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -30,8 +30,6 @@ export default class Document extends NextDocument {
       <Html lang='en'>
         <Head>
           <meta content='IE=edge' httpEquiv='X-UA-Compatible' />
-          <meta content='#262626' name='theme-color' />
-          <link rel='mask-icon' href='/favicon.svg' color='#262626' />
           <script dangerouslySetInnerHTML={{ __html: this.renderSnippet() }} />
         </Head>
         <body>
