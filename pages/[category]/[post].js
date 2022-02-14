80765b4dce3df7733de423d34cd1204170c64805
Braden Belcher <jbbelcher53@gmail.com>
2022-02-14 10:33:27 -0600

    remove slash

--- pages/[category]/[post].js
+++ pages/[category]/[post].js
@@ -15,7 +15,7 @@ export default function Post({ frontmatter, body, fields }) {
         description={frontmatter.subtitle ? frontmatter.subtitle : ''}
         banner={
           frontmatter.banner
-            ? `https://docs.planetscale.com/${frontmatter.banner}`
+            ? `https://docs.planetscale.com${frontmatter.banner}`
             : `https://og-image.planetscale.com/${encodedTitle}.png?theme=dark&direction=row&md=1&fontSize=100px&images=https%3A%2F%2Fog-image.planetscale.com%2Fimages%2Fps-logo-white.svg`
         }
         pathname={`${fields.slug}`}
