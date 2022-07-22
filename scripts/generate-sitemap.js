884a76ca3086c63bb1fd4d1a677178ce9c0c1a7b
Ayrton De Craene <hello@ayrton.be>
2022-07-22 19:42:34 +0200

    Reference the canonical URL

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -29,7 +29,7 @@ const meta = require('../content/docs/meta.json')
   const sitemap = `
         <?xml version="1.0" encoding="UTF-8"?>
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-          <url><loc>https://${baseURL}/</loc></url>
+          <url><loc>https://${baseURL}</loc></url>
           ${meta.order
             .map((category) => {
               return category.pages
