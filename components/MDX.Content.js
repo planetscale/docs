f52cc1163ea6e206a286a6f3698f03fd98c68ff6
Ayrton De Craene <hello@ayrton.be>
2022-08-03 12:34:34 +0200

    Render optimized images

--- components/MDX.Content.js
+++ components/MDX.Content.js
@@ -25,6 +25,7 @@ export default function MDXContent({ title, subtitle, banner, body, lastUpdatedO
     h3: (props) => <AnchorLink {...props} heading='h3' category={category} />,
     NextBlock,
     InfoBlock,
+    ImageBlock,
     VideoBlock,
     GetHelp
   }
--- components/MDX.ImageBlock.js
+++ components/MDX.ImageBlock.js
@@ -1,12 +1,23 @@
 import React, { useState } from 'react'
 
+import Image from 'next/image'
+
 export default function ImageBlock(props) {
   const { alt, src } = props
   const [imageURL] = useState(src)
 
+  if (!props.height || !props.width) {
+    return (
+      <span className='block mt-3'>
+        <img src={imageURL} alt={alt} className='max-h-[600px] mx-auto'></img>
+        {alt && <span className='block mt-1 mb-4 text-sm text-center text-secondary'>{alt}</span>}
+      </span>
+    )
+  }
+
   return (
     <span className='block mt-3'>
-      <img src={imageURL} alt={alt} className='max-h-[600px] mx-auto'></img>
+      <Image {...props} className='max-h-[600px] mx-auto' layout='responsive' />
       {alt && <span className='block mt-1 mb-4 text-sm text-center text-secondary'>{alt}</span>}
     </span>
   )
