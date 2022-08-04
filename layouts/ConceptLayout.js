8269bfb3915e7df24678eb91ee08da8cc5d849c7
Ayrton De Craene <hello@ayrton.be>
2022-08-05 00:03:47 +0200

    File per post

---
+++ layouts/ConceptLayout.js
@@ -0,0 +1,63 @@
+import { MDXProvider } from '@mdx-js/react'
+
+import FeedbackBlock from '../components/FeedbackBlock'
+import Footer from '../components/Footer'
+import GetHelp from '../components/GetHelp'
+import HeadingBlock from '../components/HeadingBlock'
+import Layout from '../components/layout'
+import AnchorLink from '../components/MDX.AnchorLink'
+import CodeBlock from '../components/MDX.CodeBlock'
+import ImageBlock from '../components/MDX.ImageBlock'
+import InfoBlock from '../components/MDX.InfoBlock'
+import InlineCodeBlock from '../components/MDX.InlineCodeBlock'
+import NextBlock from '../components/MDX.NextBlock'
+import TableBlock from '../components/MDX.TableBlock'
+import VideoBlock from '../components/MDX.VideoBlock'
+import PageInfo from '../components/PageInfo'
+import { TitleAndMetaTags } from '../components/TitleAndMetaTags'
+
+const components = {
+  table: TableBlock,
+  pre: CodeBlock,
+  code: InlineCodeBlock,
+  img: ImageBlock,
+  h2: (props) => <AnchorLink {...props} heading='h2' />,
+  h3: (props) => <AnchorLink {...props} heading='h3' />,
+  NextBlock,
+  InfoBlock,
+  VideoBlock,
+  GetHelp
+}
+
+export default function ConceptLayout({ children, title, subtitle, banner }) {
+  const bannerUrl = buildBannerUrl({ banner, title })
+
+  return (
+    <MDXProvider components={components}>
+      <TitleAndMetaTags title={title} subtitle={subtitle} banner={bannerUrl} />
+
+      <Layout>
+        <div className='mdx-content'>
+          <HeadingBlock title={title} subtitle={subtitle} banner={banner} />
+
+          {children}
+
+          <FeedbackBlock />
+          <PageInfo lastUpdatedOn={null && lastUpdatedOn} slug={null && slug}></PageInfo>
+        </div>
+
+        <Footer />
+      </Layout>
+    </MDXProvider>
+  )
+}
+
+function buildBannerUrl({ banner, title }) {
+  if (banner) {
+    return `https://planetscale.com/docs${banner}`
+  }
+
+  const encodedTitle = encodeURI(title)
+
+  return `https://og-image.planetscale.com/${encodedTitle}.png?theme=dark&direction=row&md=1&fontSize=100px&images=https%3A%2F%2Fog-image.planetscale.com%2Fimages%2Fps-logo-white-v2.svg`
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,42 +1,62 @@
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx$/
+import MDX from '@next/mdx'
+import withFrontmatter from 'remark-frontmatter'
+import withGitHubFlavoredMarkdown from 'remark-gfm'
+import withMdxFrontmatter from 'remark-mdx-frontmatter'
+
+const withMDX = MDX({
+  extension: /\.mdx$/,
+  options: {
+    remarkPlugins: [withFrontmatter, withGitHubFlavoredMarkdown, withMdxFrontmatter],
+    rehypePlugins: [],
+    providerImportSource: '@mdx-js/react'
+  }
 })
 
-module.exports = withMDX({
-  pageExtensions: ['js', 'jsx', 'md', 'mdx']
-})
+// const fallbackLayouts = {
+//   // 'src/pages/docs/**/*': ['@/layouts/DocumentationLayout', 'DocumentationLayout'],
+// }
+//
+// const fallbackDefaultExports = {
+//   // 'src/pages/{docs,components}/**/*': ['@/layouts/ContentsLayout', 'ContentsLayout'],
+//   // 'src/pages/blog/**/*': ['@/layouts/BlogPostLayout', 'BlogPostLayout'],
+// }
+//
+// const fallbackGetStaticProps = {
+//   // 'src/pages/blog/**/*': '@/layouts/BlogPostLayout',
+// }
+//
+// const { NODE_ENV } = process.env
+// import segment = require('./lib/segment')
+// const segmentInlineSHA = `sha256-${segment.SegmentSnippetSHA256}`
+//
+// function csp() {
+//   const isDev = NODE_ENV === 'development'
+//
+//   // There's no good way right now to compute this at build time
+//   // so this has to be manually kept in sync with the `next-themes` NPM package.
+//   const nextThemeInlineScriptHash = 'sha256-GtjUtmgtscBVAFveDly2Ug+LL+cy4ZbxFsH2nbefPAo='
+//
+//   const policies = [
+//     "base-uri 'self'",
+//     'block-all-mixed-content',
+//     "default-src 'self'",
+//     "frame-src 'none'",
+//     `connect-src 'self' https://cdn.segment.com https://api.segment.io https://*.algolia.net https://*.algolianet.com ${
+//       isDev && 'ws:'
+//     }`,
+//     `script-src 'self' '${segmentInlineSHA}' '${nextThemeInlineScriptHash}' https://cdn.jsdelivr.net https://cdn.segment.com 'unsafe-eval'`,
+//     "style-src 'self' https://cdn.jsdelivr.net 'unsafe-inline'",
+//     "img-src 'self' https://www.netlify.com https://vercel.com data:",
+//     "prefetch-src 'self'",
+//     `form-action 'self'`,
+//     "frame-ancestors 'none'"
+//   ]
+//   return policies.join('; ')
+// }
+// const CONTENT_SECURITY_POLICY = csp()
+const CONTENT_SECURITY_POLICY = ''
 
-const { NODE_ENV } = process.env
-const segment = require('./lib/segment')
-const segmentInlineSHA = `sha256-${segment.SegmentSnippetSHA256}`
-
-function csp() {
-  const isDev = NODE_ENV === 'development'
-
-  // There's no good way right now to compute this at build time
-  // so this has to be manually kept in sync with the `next-themes` NPM package.
-  const nextThemeInlineScriptHash = 'sha256-GtjUtmgtscBVAFveDly2Ug+LL+cy4ZbxFsH2nbefPAo='
-
-  const policies = [
-    "base-uri 'self'",
-    'block-all-mixed-content',
-    "default-src 'self'",
-    "frame-src 'none'",
-    `connect-src 'self' https://cdn.segment.com https://api.segment.io https://*.algolia.net https://*.algolianet.com ${
-      isDev && 'ws:'
-    }`,
-    `script-src 'self' '${segmentInlineSHA}' '${nextThemeInlineScriptHash}' https://cdn.jsdelivr.net https://cdn.segment.com 'unsafe-eval'`,
-    "style-src 'self' https://cdn.jsdelivr.net 'unsafe-inline'",
-    "img-src 'self' https://www.netlify.com https://vercel.com data:",
-    "prefetch-src 'self'",
-    `form-action 'self'`,
-    "frame-ancestors 'none'"
-  ]
-  return policies.join('; ')
-}
-const CONTENT_SECURITY_POLICY = csp()
-
-module.exports = {
+export default withMDX({
   basePath: '/docs',
   eslint: {
     ignoreDuringBuilds: true
@@ -46,9 +66,10 @@ module.exports = {
       allowFutureImage: true
     }
   },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      require('./scripts/generate-sitemap')
+  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+  webpack: (config, options) => {
+    if (options.isServer) {
+      // require('./scripts/generate-sitemap')
     }
 
     return config
@@ -174,4 +195,4 @@ module.exports = {
       }
     ]
   }
-}
+})
