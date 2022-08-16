edcaebc9f667ff4ed00eae348471f4a1750c9616
skullface <skullface@users.noreply.github.com>
2022-08-16 14:38:24 -0400

    semantics

--- components/Footer.js
+++ components/Footer.js
@@ -3,14 +3,20 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className='mt-4 flex items-center justify-between'>
-      <span>
-        <a className='mr-1.5 text-secondary hover:text-blue' href='/legal/privacy'>
-          Privacy
-        </a>
-        <a className='text-secondary hover:text-blue' href='/legal/siteterms'>
-          Terms
-        </a>
-      </span>
+      <nav>
+        <ul className='flex gap-x-1.5'>
+          <li>
+            <a className='text-secondary hover:text-blue' href='/legal/privacy'>
+              Privacy
+            </a>
+          </li>
+          <li>
+            <a className='text-secondary hover:text-blue' href='/legal/siteterms'>
+              Terms
+            </a>
+          </li>
+        </ul>
+      </nav>
       <span className='text-secondary'>&copy; 2022 PlanetScale Inc.</span>
     </div>
   )
