1d808b1806592994bfdd2a9bb7e1872faeef5f34
William Gordon <gordon@planetscale.com>
2022-09-06 13:37:55 -0400

    Real code to identify user

--- components/SegmentPageTracker.js
+++ components/SegmentPageTracker.js
@@ -5,7 +5,7 @@ import Router from 'next/router'
 // Manully track client-side page views with Segment if the user is logged in
 // GTM will track all page views via a page view tag
 Router.events.on('routeChangeComplete', (url) => {
-  if (window.analytics.identified) {
+  if (window.analytics.user().id()) {
     window.analytics.page(url)
   }
 })
