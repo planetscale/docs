9bf3c2f838c253a7bab4e0a9e0646328226152f7
Brian Morrison II <brian@brianmorrison.me>
2022-09-14 10:14:16 -0500

    use tailwind, inject vimeo script from comp

--- components/VimeoEmbed.js
+++ components/VimeoEmbed.js
@@ -1,35 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function VimeoEmbed(props) {
   const { alt, src } = props
 
-  const wrapperStyle = {
-    width: '100%',
-    position: 'relative',
-    height: 0,
-    paddingBottom: '56.25%',
-    borderRadius: '5px',
-    marginBottom: '15px'
-  }
-
-  const iframeStyle = {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    width: '100%',
-    height: '100%',
-    borderRadius: '5px'
-  }
+  useEffect(() => {
+    const script = document.createElement('script')
+    script.src = 'https://player.vimeo.com/api/player.js'
+    script.async = true
+    document.body.appendChild(script)
+    return () => {
+      document.body.removeChild(script)
+    }
+  }, [])
 
   return (
     <div className='mt-3'>
-      <div style={wrapperStyle}>
+      <div className='mb-3 w-100 relative h-0 rounded' style={{ paddingBottom: '56.25%' }}>
         <iframe
           src={src}
           frameBorder='0'
           allow='autoplay; fullscreen; picture-in-picture'
           allowFullScreen
-          style={iframeStyle}
+          className='absolute top-0 left-0 w-full h-full rounded'
           title={alt}
         ></iframe>
       </div>
